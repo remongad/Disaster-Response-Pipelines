a506e6415b4772b22b889ae22d7132b53eb0ddcb
remongad <65246412+remongad@users.noreply.github.com>
2020-05-29 00:52:41 +0200

    implement load_data, clean_data and save_data functions

--- data/process_data.py
+++ data/process_data.py
@@ -1,16 +1,47 @@
 import sys
-
+import pandas as pd
+from sqlalchemy import create_engine
 
 def load_data(messages_filepath, categories_filepath):
-    pass
+    
+    # load messages dataset
+    messages = pd.read_csv(messages_filepath)
 
+    # load categories dataset
+    categories = pd.read_csv(categories_filepath)
+
+    # merge datasets
+    df = messages.merge(categories, how='outer', on=['id', 'id'])
+
+    return df
 
 def clean_data(df):
-    pass
+    categories = df['categories'].str.split(';', expand = True)
+    # select the first row of the categories dataframe
+    row = categories.iloc[0]
+    # use this row to extract a list of new column names for categories.
+    category_colnames = [val.split('-')[0] for val in list(row.values)]
+    # rename the columns of `categories`
+    categories.columns = category_colnames
+
+    for column in categories:
+        # set each value to be the last character of the string
+        categories[column] = categories[column].apply(lambda x : x.split('-')[1])
+        
+        # convert column from string to numeric
+        categories[column] = categories[column].astype(int) 
+
+    # drop the original categories column from `df`
+    df = df.drop('categories', axis = 1)
+    # concatenate the original dataframe with the new `categories` dataframe
+    df = pd.concat([df, categories], axis = 1)
+    df.drop_duplicates(inplace=True)
+    return df
 
 
 def save_data(df, database_filename):
-    pass  
+    engine = create_engine(f'sqlite:///{database_filename}')
+    df.to_sql('cleaned_data', engine, index=False)  
 
 
 def main():
