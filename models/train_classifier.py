092a5fe72ed0a1aa8091a648005fc543ab12c13f
remongad <65246412+remongad@users.noreply.github.com>
2020-05-29 06:04:28 +0200

    add doc strings to train_classifier.py

--- models/train_classifier.py
+++ models/train_classifier.py
@@ -15,21 +15,53 @@ from sklearn.metrics import classification_report
 from sklearn.externals import joblib
 
 def load_data(database_filepath):
+    """
+    load data from specified path
+    Args:
+        database_filepath: str
+    Returns:
+        tuple of the feature and labels dataframes and list of the categories unique names
+    """
+    # load data from the database filename
     engine = create_engine(f'sqlite:///{database_filepath}')
     df = pd.read_sql_table('cleaned_data', engine)
+    
+    # split the dataframe to features and labels
     X = df['message']
     Y = df[df.columns[4:]]
     return X, Y, Y.columns.tolist()
 
 def tokenize(text):
+    """
+    normalize the text to all lower case and remove any non letter or numbers characters then add nltk lemmatization to each text token
+
+    Args:
+        text: str
+    Return:
+        list of cleansed and lemmatized tokens
+    """
+    # lowercase the text and keep text that only contains letters and numbers
     text = re.sub('[^a-z0-9]', ' ', text.lower())
+
+    # split text to list
     tokens = nltk.word_tokenize(text.strip())
+
+    # applying nltk lemmatization to each token
     lemmatizer = nltk.WordNetLemmatizer() 
     tokens_lemmatized = [lemmatizer.lemmatize(tok) for tok in tokens]
+
     return tokens_lemmatized
 
 
 def build_model():
+    """
+    it create sklearn pipeline to create tf-idf vectors then pass the result to multi output classifier that use random forest to 
+    build a model for mutli label classification
+
+    Returns:
+        pipeline object that has both fit and predict methods. 
+    """
+    # create sklearn pipeline to train Random forest model on tf-idf vectors
     return Pipeline([
     ('tfidf', TfidfVectorizer(tokenizer=tokenize)),
     ('clf', MultiOutputClassifier(RandomForestClassifier(min_samples_split = 2)))
@@ -37,7 +69,21 @@ def build_model():
 
 
 def evaluate_model(model, X_test, Y_test, category_names):
+    """
+    predict the model output on the test data and evaluate the result using 'classification_report' in sklearn
+    Args:
+        model: sklearn estimator
+        X_test: pandas dataframe
+        Y_test: pandas dataframe
+        category_names: list:
+    Returns:
+        it print the f-score, precision and recall for each column in the model prediciton output.
+    """
+
+    # get prediction on the test data
     y_pred = model.predict(X_test)
+
+    # display f-score, precision, recall for each column in the labels
     for i, column_name in enumerate(category_names):
         print(f'This Classification Report for {column_name}')
         print(classification_report(Y_test.iloc[:,i], y_pred[:,i]))
@@ -45,6 +91,15 @@ def evaluate_model(model, X_test, Y_test, category_names):
 
 
 def save_model(model, model_filepath):
+    """
+    save an sklearn model to pkl file for future loading
+    Args:
+        model: sklearn estimator
+        model_filepath: str: path to save the model
+    Returns:
+        it saves the input model to the specified path
+    """
+    # save the model in pkl format
     joblib.dump(model, model_filepath)
 
 
